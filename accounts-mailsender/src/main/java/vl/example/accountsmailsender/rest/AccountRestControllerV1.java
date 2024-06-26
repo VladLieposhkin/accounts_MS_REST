3f353710426a58f56c790ae22ecbb3de4150ea63
Vlad Lieposhkin <Vlad.Lieposhkin@gmail.com>
2024-06-26 11:06:45 +0100

    bf commit

--- accounts-mailsender/src/main/java/vl/example/accountsmailsender/rest/AccountRestControllerV1.java
+++ accounts-mailsender/src/main/java/vl/example/accountsmailsender/rest/AccountRestControllerV1.java
@@ -5,6 +5,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,6 +14,8 @@ import vl.example.accountscommon.dto.AccountDTO;
 import vl.example.accountscommon.dto.ClientDTO;
 
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @RestController
 @Slf4j
@@ -20,24 +23,38 @@ import java.util.List;
 @RequestMapping("/api/v1/mail")
 public class AccountRestControllerV1 {
 
-//    private JavaMailSender mailSender;
+    private final JavaMailSender javaMailSender;
+
     @PostMapping
     public ResponseEntity<Integer> sendMail(@RequestBody List<AccountDTO> accountDTOs) {
 
-        List<ClientDTO> clients = accountDTOs.stream()
-                .map(AccountDTO::getClient)
-                .distinct()
-                .peek(clientDTO -> {
-                    SimpleMailMessage message = new SimpleMailMessage();
-                    message.setTo(clientDTO.getEmail());
-                    message.setSubject("Action is required");
-                    message.setText("Dear " + clientDTO.getName() + "! Accounts: ...");
+        Map<ClientDTO, List<AccountDTO>> mails = accountDTOs.stream()
+                .collect(Collectors.groupingBy(AccountDTO::getClient));
 
-//                    mailSender.send(message);
+        for (Map.Entry<ClientDTO, List<AccountDTO>> entry : mails.entrySet()) {
 
-                    log.info("Mail to client {} has been sent", clientDTO.getName());
-                })
-                .toList();
-        return new ResponseEntity<>(clients.size(), HttpStatus.OK);
+            ClientDTO client = entry.getKey();
+            List<AccountDTO> accounts = entry.getValue();
+
+            SimpleMailMessage mailMessage = new SimpleMailMessage();
+
+            mailMessage.setTo(client.getEmail());
+            mailMessage.setSubject("Action is required");
+            mailMessage.setText("Dear " + client.getName() + "\n" +
+                    accounts.stream()
+                            .map(account ->
+                                    "Account: " + account.getNumber() +
+                                            ", Coin : " + account.getCoin().getName() +
+                                            ", Change : " + account.getCoin().getChange7d())
+                            .collect(Collectors.joining("\n")) + "\nBest regards");
+
+//            javaMailSender.send(mailMessage);
+
+            log.info("MAIL PROCESSOR. Mail processing: Mail to client {} has been sent", client.getName());
+        }
+        log.info("MAIL PROCESSOR. Mail processing: Data about {} accounts is processed", accountDTOs.size());
+
+        return new ResponseEntity<>(accountDTOs.size(), HttpStatus.OK);
     }
+
 }
